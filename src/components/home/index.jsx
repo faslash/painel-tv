1c462aa68b75b8cca99cf7aaafd59696ba10b709
faslash <bicadete@hotmail.com>
2021-04-02 20:03:50 -0300

    Layout Atualizado!

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Clock from './clock';
 import './index.css';
 
 export default () => {
     return(
         <div className="senha">
             <div className="box-tv">
-                <iframe title="tv" width="100%" height="100%" src="chrome-extension://ckblfoghkjhaclegefojbgllenffajdc/player.html#https://manifest.googlevideo.com/api/manifest/hls_variant/expire/1616984823/ei/l-ZgYI7sEN-a1sQP3OKmiAw/ip/138.121.63.18/id/5qap5aO4i9A.0/source/yt_live_broadcast/requiressl/yes/hfr/1/playlist_duration/30/manifest_duration/30/maxh/2160/maudio/1/vprv/1/go/1/nvgoi/1/keepalive/yes/fexp/24001373%2C24007246/dover/11/itag/0/playlist_type/DVR/sparams/expire%2Cei%2Cip%2Cid%2Csource%2Crequiressl%2Chfr%2Cplaylist_duration%2Cmanifest_duration%2Cmaxh%2Cmaudio%2Cvprv%2Cgo%2Citag%2Cplaylist_type/sig/AOq0QJ8wRgIhAKRMoL3w11XV0OMwBFLU1v9xXx-ZHwUg4iiID7mApMT0AiEAn83sobCH160TWGpbKCkQ3mZLwUsOuWzQLvIqSxl3Mnk%3D/file/index.m3u8"></iframe>
+                <iframe title="tv" width="100%" height="100%" src="chrome-extension://ckblfoghkjhaclegefojbgllenffajdc/player.html#https://manifest.googlevideo.com/api/manifest/hls_variant/expire/1617425969/ei/0aFnYL3AJ5qa1sQP-qewiAU/ip/131.72.203.195/id/CVGjhpUxRzg.1/source/yt_live_broadcast/requiressl/yes/hfr/1/playlist_duration/30/manifest_duration/30/maxh/2160/maudio/1/vprv/1/go/1/nvgoi/1/keepalive/yes/fexp/24001373%2C24007246/dover/11/itag/0/playlist_type/DVR/sparams/expire%2Cei%2Cip%2Cid%2Csource%2Crequiressl%2Chfr%2Cplaylist_duration%2Cmanifest_duration%2Cmaxh%2Cmaudio%2Cvprv%2Cgo%2Citag%2Cplaylist_type/sig/AOq0QJ8wRAIgOz3Pv5v6QMBJb3W4bs7txJl9yt-oBFWwyiwnbScGiZQCIDyAxo3kVmcwzXHUDX7xabgIYDM261Xg4DQ4aCMVj3Ty/file/index.m3u8"></iframe>
             </div>
 
             <div className="barra-lateral">
@@ -14,6 +15,7 @@ export default () => {
                     <h1>S005</h1>
                 </div>
 
+                <Clock clock="ClockData"></Clock>
             
             </div>
 
