666ddfef0891991d8baf281a50e02b52189d7106
faslash <bicadete@hotmail.com>
2021-05-14 08:47:51 -0300

    Voltando ao normal!

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const senhaRouter = require("./routes/Senha");
 app.use("/senhas", senhaRouter);
 
 // WebSocket
-server.listen(3001, '192.168.0.226',() => {
+server.listen(3001, () => {
   console.log("Servidor rodando na porta 3001");
 });
 
